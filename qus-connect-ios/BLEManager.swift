fc282f40b4ffd19d0a31d5cac788f3cb764d3060
Vedran Erak <vedran1.erak1@gmail.com>
2025-10-17 17:40:49 +0200

    Enable notifications after characteristics discovery

--- qus-connect-ios/BLEManager.swift
+++ qus-connect-ios/BLEManager.swift
@@ -98,11 +98,18 @@ class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeriph
         
         peripheral.delegate = self
         
-        let connectedDevice = scannedDevices.first(where: { $0.peripheral.identifier == peripheral.identifier })!
+        guard let connectedDeviceWrapper = scannedDevices.first(where: { $0.peripheral.identifier == peripheral.identifier }) else {
+            print("Error: Connected peripheral not found in scanned devices list.")
+            // Disconnect if this happens, unexpected state.
+            central.cancelPeripheralConnection(peripheral)
+            return
+        }
         
-        connectedDevices.append(connectedDevice)
+        if !connectedDevices.contains(where: { $0.peripheral.identifier == connectedDeviceWrapper.peripheral.identifier }) {
+            connectedDevices.append(connectedDeviceWrapper)
+        }
         
-        peripheral.discoverServices([])
+        peripheral.discoverServices(nil)
     }
     
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
@@ -117,19 +124,38 @@ class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeriph
     // MARK: - CBPeripheralDelegate Methods
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         guard let services = peripheral.services else { return }
+        
         for service in services {
             print("Discovering characteristics for service: \(service.uuid.uuidString)")
+            peripheral.discoverCharacteristics(nil, for: service)
         }
     }
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         guard let characteristics = service.characteristics else { return }
-        for characteristic in characteristics {
-            print("Characteristic: \(characteristic)")
+        print("Discovered characteristics for service: \(service.uuid.uuidString)")
+        
+        // enable notifications after discovering characteristics
+        switch service.uuid {
+        case SensorType.OBU.SERVICE_NUS_SERVICE_ID:
+            guard let obuNusTxCharacteristic = characteristics.first(where: { $0.uuid == SensorType.OBU.SERVICE_NUS_TX_ID }) else {return}
+            print("Enabling notifications for OBU NUS TX characteristic")
+            peripheral.setNotifyValue(true, for: obuNusTxCharacteristic)
+        case SensorType.CORE.SERVICE_TEMPERATURE_SERVICE_ID:
+            guard let coreBodyTempNotificationCharacteristic = characteristics.first(where: { $0.uuid == SensorType.CORE.SERVICE_TEMPERATURE_TEMP_ID }) else { return }
+            print("Enabling notifications for CORE Body Temperature characteristic")
+            peripheral.setNotifyValue(true, for: coreBodyTempNotificationCharacteristic)
+            guard let coreBodyControlPointCharacteristic = characteristics.first(where: { $0.uuid == SensorType.CORE.SERVICE_TEMPERATURE_CONTROL_POINT_ID }) else { return }
+            print("Enabling notifications for control point as it does not accept writes without notification enabled!!!")
+            peripheral.setNotifyValue(true, for: coreBodyControlPointCharacteristic)
+        default:
+            return
         }
     }
     
-    //    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-    //
-    //    }
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        guard let data = characteristic.value else { return }
+        
+        print("Received value for characteristic \(characteristic.uuid): \(data)")
+    }
 }
